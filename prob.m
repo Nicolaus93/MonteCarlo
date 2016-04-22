255cd426a229d5220e776cb820fad0c8ff55a7a3
Nicolaus93 <nico.campolongo@gmail.com>
2016-04-22 20:50:39 +0200

    Update prob.m

--- prob.m
+++ prob.m
@@ -9,8 +9,3 @@ function p = prob(obs, part1, part4)
     mean = 90-30*log10(sqrt(one.^2 + two.^2));   % Nx7
     y = repmat(obs,length(part1),1); % should be Nx7
     p = prod(normpdf(y,mean,1.5^2),2);
-    %p = sum(p);
-    
-    %p = @(y,x) mvnpdf(y,v - ...
-    %    10*eta*log10(sqrt(sum(bsxfun(@minus,x',stations).^2,1))'), ...
-    %                    eye(7)*1.5^2);
