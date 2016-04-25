62734d2d3e24e3834a3149a0fcaf5817f62b79f3
nicoloc <nico.campolongo@gmail.com>
2016-04-25 09:49:47 +0200

    pff

--- SIS2.m
+++ SIS2.m
@@ -11,25 +11,6 @@ function [tau, w] = SIS2(obs)
     % initial weights
     w = prob(obs(:,1)', part(1,:), part(4,:));
     
-    %{
-    indices = randi(5,1,N);
-    % should define tau(1)
-    bigZ = zeros(2,N);
-    
-    % nice version
-    for k = 1:n
-        for j = 1:5
-            temp = indices;
-            indices(indices==j) = randsample(5,sum(indices==j),true,trans(j,:));
-            bigZ(:,temp==j) = Z(:,indices(temp==j));
-        end
-        part = phi*part + psiZ*bigZ + psiW * mvnrnd(zeros(2,1),.5^2*ones(2),N)';
-        w = w.*prob(obs(:,k+1)', part(1,:), part(4,:));
-        k %print the timesteps
-        tau(:,k+1) = sum(bsxfun(@times,part,w'),2)/sum(w);
-    end
-    %}
-    
     % transition matrices
     temp = zeros(N,5);
     indices = randi(5,1,N);
