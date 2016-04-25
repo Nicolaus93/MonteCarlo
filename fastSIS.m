b3aa41ee04311916826e26c9f01180609923db61
nicoloc <nico.campolongo@gmail.com>
2016-04-25 09:51:10 +0200

    to fix

--- fastSIS.m
+++ fastSIS.m
@@ -1,4 +1,4 @@
-function [tau, w] = fastSIS(obs)
+function [tau, w] = fastSIS(obs, resampling)
     global Z phi psiZ psiW trans 
     N = 10000;
     n = length(obs);
@@ -13,6 +13,7 @@ function [tau, w] = fastSIS(obs)
     
     indices = randi(5,1,N);
     % should define tau(1)
+    % tau(:,1) = 
     bigZ = zeros(2,N);
     
     % nice version
@@ -22,8 +23,13 @@ function [tau, w] = fastSIS(obs)
             indices(indices==j) = randsample(5,sum(indices==j),true,trans(j,:));
             bigZ(:,temp==j) = Z(:,indices(temp==j));
         end
-        part = phi*part + psiZ*bigZ + psiW * mvnrnd(zeros(2,1),.5^2*ones(2),N)';
-        w(:,k+1) = w(:,k).*prob(obs(:,k+1)', part(1,:), part(4,:));
+        part = phi*part + psiZ*bigZ + psiW * mvnrnd(zeros(2,1),.5^2*ones(2),N)'; % mutation
+        w(:,k+1) = w(:,k).*prob(obs(:,k+1)', part(1,:), part(4,:)); % weighting
         k %print the timesteps
-        tau(:,k+1) = sum(bsxfun(@times,part,w(:,k+1)'),2)/sum(w(:,k+1));
+        tau(:,k+1) = sum(bsxfun(@times,part,w(:,k+1)'),2)/sum(w(:,k+1)); % estimation
+        if resampling == true
+           % to do: modify w update as well
+           ind = randsample(N,N,true,w(:,k+1));
+           part = part(:,ind);
+        end
     end
