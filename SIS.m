9c39f771f7948db41729946fddfb2a33d68c1675
nicoloc <nico.campolongo@gmail.com>
2016-04-25 09:47:15 +0200

    fast version

--- SIS.m
+++ SIS.m
@@ -1,45 +1,35 @@
 function [tau, w] = SIS(obs,stations)
     global Z phi psiZ psiW trans 
     N = 10000;
-    n = length(obs)-1;
+    n = length(obs);
     tau = zeros(6,n);
+    n = n-1;
     
-    % prob dist of w
-    v = 90*ones(length(stations),1);
-    eta = 3;
-    p = @(y,x) mvnpdf(y,v - ...
-        10*eta*log10(sqrt(sum(bsxfun(@minus,x',stations).^2,1))'), ...
-                        eye(7)*1.5^2);
-                    
     % initial particle
     part = mvnrnd(zeros(6,1), diag([500,5,5,200,5,5]), N)'; % 6XN particles
     
-    % weights
-    w = zeros(N,1);
-    %test = arrayfun(@(x) p(obs(:,1),x), [part(1,:) part(4,:)]); % it's slower
+    % initial weights
+    w(:,1) = prob(obs(:,1)', part(1,:), part(4,:));
     
-    for j = 1:N
-        w(j) = p(obs(:,1), [part(1,j), part(4,j)]);
-    end
-    tau(:,1) = sum(bsxfun(@times,part,w'),2)/sum(w);
-    
-    % transition matrices
-    temp = zeros(N,5);
     indices = randi(5,1,N);
-    for i = 1:N
-        temp(i, indices(i))=1;
-    end
+    % should define tau(1)
+    % tau(:,1) = 
+    bigZ = zeros(2,N);
     
+    % nice version
     for k = 1:n
-        for j = 1:N
-            index = randsample(5,1,true,temp(j,:)*trans);
-            temp(j,:) = zeros(1,5);
-            temp(j,index) = 1;
-            part(:,j) = phi*part(:,j) + psiZ*Z(:,index) + ...
-                psiW * mvnrnd(zeros(2,1),.5^2*ones(2))';
-            w(j) = w(j).*p(obs(:,k+1), [part(1,j), part(4,j)]);
-        end 
-        k %print the time steps
-        tau(:,k+1) = sum(bsxfun(@times,part,w'),2)/sum(w);
+        for j = 1:5
+            temp = indices;
+            indices(indices==j) = randsample(5,sum(indices==j),true,trans(j,:));
+            bigZ(:,temp==j) = Z(:,indices(temp==j));
+        end
+        part = phi*part + psiZ*bigZ + psiW * mvnrnd(zeros(2,1),.5^2*ones(2),N)'; % mutation
+        w(:,k+1) = w(:,k).*prob(obs(:,k+1)', part(1,:), part(4,:)); % weighting
+        k %print the timesteps
+        tau(:,k+1) = sum(bsxfun(@times,part,w(:,k+1)'),2)/sum(w(:,k+1)); % estimation
+        if resampling == true
+           ind = randsample(N,N,true,w(:,k+1));
+           part = part(:,ind);
+        end
     end
     
