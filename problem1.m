5c19afc284e3da154a9f288559618fbf34e60ef9
nicoloc <nico.campolongo@gmail.com>
2016-04-25 11:51:52 +0200

    added problem4

--- problem1.m
+++ problem1.m
@@ -21,11 +21,11 @@ temp(randi(5)) = 1;
 
 %index = randsample(5,1,true,trans(:,randi(5)))
 
-for i = 3:100
-    % index = randsample(5,1,true,trans(index,:);
-    index = randsample(5,1,true,temp*trans);
-    temp = zeros(1,5);
-    temp(index) = 1;
+for i = 3:500
+    index = randsample(5,1,true,trans(index,:));
+    %index = randsample(5,1,true,temp*trans);
+    %temp = zeros(1,5);
+    %temp(index) = 1;
     X(:,i) = phi*X(:,i-1) + psiZ*Z(:,index) + ...
         psiW * mvnrnd(zeros(2,1),.5^2*ones(2))';
 end
@@ -41,35 +41,46 @@ global stations
 for i = 1:length(X)
     observ(:,i) = obs(x(i), y(i), stations);  
 end
+hold on
+scatter(stations(1,:),stations(2,:),'r')
+plot(x,y,'b')
 
 %% a test on our trajectory
 tic
-[tau, w] = fastSIS(observ);
+[tau, w] = fastSISR(observ);
 toc
 x1 = tau(1,:);
 y1 = tau(4,:);
 hold on
+scatter(stations(1,:),stations(2,:),'r')
 plot(x,y,'b')
 plot(x1,y1,'r')
 
 %% plot histograms
 figure
 subplot(4,1,1)       
-histogram(log10(w(:,1)),[-350:10:0])
+histogram(log10(w(:,1)),-350:10:0)
 title('n = 1')
 
 subplot(4,1,2)       
-histogram(log10(w(:,10)),[-350:10:0])
+histogram(log10(w(:,10)),-350:10:0)
 title('n = 10')
 
 subplot(4,1,3)       
-histogram(log10(w(:,20)),[-350:10:0])
+histogram(log10(w(:,20)),-350:10:0)
 title('n = 20')
 
 subplot(4,1,4)       
-histogram(log10(w(:,40)),[-350:10:0])
+histogram(log10(w(:,40)),-350:10:0)
 title('n = 40')
 
-%% to do
+%% problem4 trajectory estimation
 load('RSSI-measurements.mat')
+[tau, w] = fastSISR(Y);
+x1 = tau(1,:);
+y1 = tau(4,:);
 
+%% plot results
+hold on
+scatter(stations(1,:),stations(2,:),'r','filled')
+scatter(x1,y1,1,'b','filled')
